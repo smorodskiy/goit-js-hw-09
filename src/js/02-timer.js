6cb9a54c7fcb3d78144aed7d54430c1134207c55
Igor Smorodsky <ingerard@gmail.com>
2022-08-25 15:49:18 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const input = document.getElementById("datetime-picker");
 const days = document.querySelector("[data-days]");
@@ -8,17 +9,29 @@ const mins = document.querySelector("[data-minutes]");
 const secs = document.querySelector("[data-seconds]");
 
 
+Notiflix.Notify.success('Sol lucet omnibus');
+Notiflix.Notify.failure('Qui timide rogat docet negare');
+Notiflix.Notify.warning('Memento te hominem esse');
+Notiflix.Notify.info('Cogito ergo sum');
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
+    // minDate: "today",
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+        setTimer(selectedDates[0]);
     },
 };
 
 flatpickr(input, options);
+
+function setTimer(date) {
+
+    console.log(date);
+    
+}
 
 function convertMs(ms) {
     // Number of milliseconds per unit of time
